241397f2796b02fc5ee7f9ab53a9ce81f222f1be
bik3te <leny.bueno@digital.security>
2019-01-17 23:25:13 +0100

    Screenshots preview in a bootstrap gallery

--- paparazzi.py
+++ paparazzi.py
@@ -3,14 +3,13 @@ Simple HTTP(S) screenshoter using Selenium and Chrome Driver.
 """
 
 import io
-import os
 import time
 import shlex
-from pathlib import Path
 from selenium import webdriver
 from subprocess import run, PIPE
+from shutil import copytree, Error
 from multiprocessing.dummy import Pool as ThreadPool
-from utils.funcs import usage, print_info, print_warning, print_error, print_success
+from utils.funcs import usage, print_info, print_error, print_success
 
 class Paparazzi(object):
     """A really disturbing person..."""
@@ -20,7 +19,9 @@ class Paparazzi(object):
         self.nb_threads = nb_threads
         self.proxy = proxy
         self.hosts_list = [host.strip().decode('utf-8') for host in self.hosts_gen()]
+        self.screenshots = []
         self.grapeshot()
+        self.create_gallery()
 
     def hosts_gen(self):
         """Parse Nmap XML and look for hosts with HTTP(S) services."""
@@ -30,13 +31,10 @@ class Paparazzi(object):
 
     def grapeshot(self):
         """Let's go."""
-        if not Path(self.output_dir).exists():
-            print_warning('{} does not exist, creating it...'.format(self.output_dir))
-            try:
-                access_rights = 0o755
-                os.makedirs(self.output_dir, access_rights)
-            except OSError:
-                print_error('Error creating {}!')
+        try:
+            copytree('data', self.output_dir)
+        except Error as err:
+            print_error('Error creating {}, directory exist!'.format(err))
 
         with ThreadPool(self.nb_threads) as pool:
             pool.map(self.screenshot, self.hosts_list)
@@ -54,12 +52,41 @@ class Paparazzi(object):
             driver = webdriver.Chrome(desired_capabilities=desired_caps)
 
             driver.get(url)
-            output = '{}/{}.png'.format(self.output_dir, url.split('://')[1])
+            title = driver.title
+            output = '{}/images/{}.png'.format(self.output_dir, url.split('://')[1])
             driver.save_screenshot(output)
             driver.close()
             print_success('Screenshot of {} OK!'.format(url))
+            self.screenshots.append({'url': url, 'path': output.replace('{}/'.format(self.output_dir), ''), 'title': title})
         except:
             print_error('Screenshot of {} KO...'.format(url))
+
+    def create_gallery(self):
+        """Preparing the screenshot gallery!"""
+        with open('data/index.html', 'r') as fd_in:
+            template = fd_in.read()
+
+        data = ''
+        for screenshot in self.screenshots:
+            data += """<div class="col-sm-6 col-md-4">
+                          <div class="thumbnail">
+                              <a class="lightbox" href="{}">
+                                  <img src="{}" alt="{}">
+                              </a>
+                              <div class="caption">
+                                  <h3>{}</h3>
+                                  <p>{}</p>
+                              </div>
+                          </div>
+                      </div>
+                      """.format(screenshot['url'],
+                                 screenshot['path'],
+                                 screenshot['title'],
+                                 screenshot['title'],
+                                 screenshot['url'])
+
+        with open('{}/index.html'.format(self.output_dir), 'w') as fd_out:
+            fd_out.write(template.replace('[w00t]', data))
 
 if __name__ == '__main__':
     args = usage()
--- utils/funcs.py
+++ utils/funcs.py
@@ -21,7 +21,7 @@ def usage():
     """Argument parser function."""
     parser = argparse.ArgumentParser(description="Please don't visit each page manually...")
     parser.add_argument('nmap_xml', help="Nmap XML output file")
-    parser.add_argument('-o', '--output_dir', help="On which directory do you want to save screenshots? (default=./screenshots)", default='./screenshots')
+    parser.add_argument('-o', '--output_dir', help="On which directory do you want your screenshot gallery? (default=./gallery)", default='./gallery')
     parser.add_argument('-p', '--proxy', help="Do you want to use proxy? scheme://[user:password@]host:port", default=None)
     parser.add_argument('-t', '--threads', help="How many threads to use? (default=8)", type=int, default=8)
     return parser.parse_args()
